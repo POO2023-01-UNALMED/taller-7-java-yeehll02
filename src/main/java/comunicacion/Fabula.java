54d95455ed9f699cc74aa6ad7c33f04c346a4fd1
yeehll02 <116982357+yeehll02@users.noreply.github.com>
2023-04-01 15:54:01 -0500

    Talleer-7

---
+++ src/main/java/comunicacion/Fabula.java
@@ -0,0 +1,54 @@
+package comunicacion;
+
+public class Fabula extends Escrito  {
+	private String ensenanza;
+	private String interpretacion;
+	
+	public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.ensenanza=ensenanza;
+		this.interpretacion=interpretacion;
+	} 
+	
+	
+	
+	public String getEnsenanza() {
+		return ensenanza;
+	}
+
+
+
+	public void setEnsenanza(String ensenanza) {
+		this.ensenanza = ensenanza;
+	}
+
+
+
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+
+
+
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+
+
+
+	public int palabrasTotales(int palabrasPagina) {
+		return 1*getPaginas()*palabrasPagina;
+	}
+	
+	public String interpretacion() {
+		return interpretacion;
+		
+	}
+	 public String toString() {
+		 return super.getOrigen()+"\n"+ super.getTitulo()+"\n"+ super.getAutor()+"\n"+super.getPaginas()+"\n"+
+				 this.ensenanza;
+				 
+	 }
+	
+
+}
